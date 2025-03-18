500b29cd4f46d75d1b683d5f346a02f4c64c8c63
Daiana <132101450+daianaq@users.noreply.github.com>
2025-03-17 21:04:47 -0300

    Update Ex02.java

--- Exercicios-Repeticao/Ex02.java
+++ Exercicios-Repeticao/Ex02.java
@@ -7,10 +7,10 @@ public class Ex02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int i = 0, resultado;
+        int n, i = 0, resultado;
         
         System.out.println("Informe o número da tabuada desejada: ");
-        int n = sc.nextInt();
+        n = sc.nextInt();
                 
         while (i <= 10) {
             resultado = i * n;
